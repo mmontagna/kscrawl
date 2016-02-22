de001ca1b45a7ad2294b9e33a4df69797e8df6bd
Marco Montagna <marcojoemontagna@gmail.com>
2016-02-22 11:48:41 -0800

    Make it possible for crawlers to use multiple response processors.

--- crawl/crawler/Abstract.py
+++ crawl/crawler/Abstract.py
@@ -1,7 +1,7 @@
 
 class AbstractCrawler():
 
-  def __init__(self, queue, urlset, http, processor):
+  def __init__(self, queue, urlset, http):
     if ("send" not in dir(queue) or "get" not in dir(queue)):
       raise Exception('AbstractCrawler: ' + str(queue) + ' must support get & send.')
     if ("add" not in dir(urlset) or "__contains__" not in dir(urlset)):
@@ -10,4 +10,15 @@ class AbstractCrawler():
     self.queue = queue
     self.urlset = urlset
     self.http = http
-    self.processor = processor
+    self.processors = []
+
+  def add_response_processor(self, processor):
+    self.processors.append(processor)
+
+  def process(self, url, response):
+    for processor in self.processors:
+      processor.process(url, response)
+
+  def close(self):
+    for processor in self.processors:
+      processor.close()
