0b8fc914f1aa2747a6558b639be39abdeff64db5
Marco Montagna <marcojoemontagna@gmail.com>
2016-02-22 11:46:35 -0800

    Try and parse response with BeautifulSoup.

--- crawl/http/Response.py
+++ crawl/http/Response.py
@@ -1,4 +1,5 @@
-import chardet
+from bs4 import BeautifulSoup
+import chardet, time
 
 class Response:
   def __init__(self, raw, request):
@@ -6,3 +7,8 @@ class Response:
     self.encoding = chardet.detect(raw)
     self.content = unicode(raw, self.encoding['encoding'])
     self.request = request
+    self.accessed = int(time.time())
+    try:
+      self.soup = BeautifulSoup(self.content, "html.parser")
+    except:
+      pass
