6b2efc8389dab0fb3c933afd05d1890bbbf876d5
Marco Montagna <marcojoemontagna@gmail.com>
2016-02-22 11:44:52 -0800

    Make LinkCrawlRequest track depth of search.

--- crawl/LinkCrawlRequest.py
+++ crawl/LinkCrawlRequest.py
@@ -1,10 +1,11 @@
 
 class LinkCrawlRequest:
 
-  def __init__(self, link, crawl_id):
+  def __init__(self, link, crawl_id = 0, depth = 0):
     self.link = link
     self.attempts = 0
     self.crawl_id = crawl_id
+    self.depth = depth
 
   def addAttempt(self):
     self.attempts += 1
