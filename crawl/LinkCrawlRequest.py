935424e920c97b14d08e8a0cf1ef010f75933e49
Marco Montagna <marcojoemontagna@gmail.com>
2016-02-23 01:40:40 -0800

    Pull in URL normalization logic into the LinkCrawlRequest.

--- crawl/LinkCrawlRequest.py
+++ crawl/LinkCrawlRequest.py
@@ -1,12 +1,16 @@
+import urlnorm, urlparse
 
 class LinkCrawlRequest:
 
-  def __init__(self, link, crawl_id = '0', depth = 0):
-    self.link = link
+  def __init__(self, link, parent = None, crawl_id='0'):
+    self.link = urlnorm.norm(urlparse.urljoin(parent.link if parent else '', link))
     self.attempts = 0
-    self.crawl_id = crawl_id
-    self.depth = depth
+    self.crawl_id = parent.crawl_id if parent else crawl_id
+    self.depth = parent.depth if parent else 0
 
   def addAttempt(self):
     self.attempts += 1
+
+  def id(self):
+    return self.crawl_id + ':' + self.link
 
