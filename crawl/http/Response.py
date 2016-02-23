8df97ba04ba635775d388a4515f7ab79912f0e17
Marco Montagna <marcojoemontagna@gmail.com>
2016-02-22 23:31:38 -0800

    Add a default response output object to all requests.

--- crawl/http/Response.py
+++ crawl/http/Response.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import chardet, time
 
+from crawl.processors import ResponseOutput
+
 class Response:
   def __init__(self, raw, request):
     self.raw = raw
@@ -9,6 +11,8 @@ class Response:
     self.request = request
     self.accessed = int(time.time())
     self.output = []
+    self.output.append(ResponseOutput('content', 'pages', self.content))
+
     try:
       self.soup = BeautifulSoup(self.content, "html.parser")
     except:
