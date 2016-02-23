cfe1c030c55e0fd1cfc0678181358521590bd686
Marco Montagna <marcojoemontagna@gmail.com>
2016-02-22 16:31:21 -0800

    Generalize the store to S3 response processor.

--- crawl/processors/S3.py
+++ crawl/processors/S3.py
@@ -4,46 +4,49 @@ import uuid, cStringIO, gzip, json, boto3, os, datetime
 from urlparse import urlparse
 
 class S3Store(AbstractProcessor):
-  def __init__(self, bucket, buffer_size=100):
-    self.output = defaultdict(lambda: defaultdict(lambda : {}))
-    self.aux_out = defaultdict(lambda: defaultdict(lambda : {}))
+  def __init__(self, bucket, buffer_size=100, s3_bucket_object=None):
+    #domain.crawl_id.folder.name
+    self.output = defaultdict(lambda: defaultdict(lambda : defaultdict(lambda : defaultdict(lambda : []))))
+    self.max_buffer_size = buffer_size
+
     self.bucket = bucket
-    self.s3 = None
-    self.buffer_size = buffer_size
+    if (s3_bucket_object is not None):
+      self.s3_bucket_object = s3_bucket_object
+    else:
+      self.s3_bucket_object = boto3.resource('s3').Bucket(self.bucket)
 
   def process(self, url, response):
     domain = urlparse(url).netloc
-    cleaned = unicode(response.raw, errors='replace')
-    self.output[domain][response.request.crawl_id][url] = {'content' : cleaned, 'accessed' : response.accessed}
-    self.aux_out[domain][response.request.crawl_id][url] = {'feature_hash' : response.feature_vector, 'accessed' : response.accessed}
+    cleaned = unicode(response.raw, errors='replace') ## USE  THIS IN EARLIER PROCESSOR STEP
+    for output in response.output:
+      self.output[domain][response.request.crawl_id][output.folder][output.name].append(output)
+
     self.checkBuffer()
+
+  def buffer_size(self, domain, crawl_id):
+    source = self.output[domain][crawl_id]
+    return max([len(source[key][key2]) for key, key2 in [(key, key2) for key in source for key2 in source[key]]])
 
   """ Check all buffers and flush if full. """
   def checkBuffer(self):
-    for group in self.output:
-      for crawl_id in self.output[group]:
-        if (len(self.output[group][crawl_id].keys()) > self.buffer_size):
-          self.write(group, crawl_id)
+    for domain, crawl_id in [(domain, crawl_id) for domain in self.output for crawl_id in self.output[domain]]:
+      if (self.buffer_size(domain, crawl_id) > self.max_buffer_size):
+        self.write(domain, crawl_id)
 
   def write(self, group, crawl_id):
-    self.writeToS3(group, 'content', 'object', crawl_id, json.dumps(self.output[group][crawl_id]))
-    self.writeToS3(group, 'auxiliary', 'index', crawl_id, json.dumps(self.aux_out[group][crawl_id]))
-    self.aux_out[group][crawl_id] = {}
+    for folder, name in [(folder, name) for folder in self.output[group][crawl_id] for name in self.output[group][crawl_id][folder]]:
+      print 'writing', group, crawl_id, folder, name
+      content = json.dumps([x.content for x in self.output[group][crawl_id][folder][name]])
+      self.writeToS3(group, folder, name, crawl_id, content)
+    self.output[group][crawl_id] = []
 
   def writeToS3(self, group, folder, name, crawl_id, content):
-    if (not self.s3):
-      self.s3 = boto3.resource('s3')
-
     object_id = str(uuid.uuid4())
-    key = os.path.join(group, crawl_id, folder, object_id + '.' + name + '.json.gz')
-
+    key = os.path.join(group, crawl_id, folder, name + '.' + object_id + '.json.gz')
     fgz = cStringIO.StringIO()
     with gzip.GzipFile(mode='wb', fileobj=fgz) as gzip_obj:
         gzip_obj.write(content)
-
-    print "Writing", name, object_id, group, "for crawl", crawl_id
-
-    self.s3.Bucket(self.bucket).Object(key).put(Body=fgz.getvalue())
+    self.s3_bucket_object.Object(key).put(Body=fgz.getvalue())
 
   def close(self):
     for group in self.output:
