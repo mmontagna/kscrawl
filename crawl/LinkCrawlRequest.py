4a861589a4183ec90d47feba6e0e2e15c781787f
Marco Montagna <marcojoemontagna@gmail.com>
2016-02-22 16:30:28 -0800

    Make crawl id defaults to a string.

--- crawl/LinkCrawlRequest.py
+++ crawl/LinkCrawlRequest.py
@@ -1,7 +1,7 @@
 
 class LinkCrawlRequest:
 
-  def __init__(self, link, crawl_id = 0, depth = 0):
+  def __init__(self, link, crawl_id = '0', depth = 0):
     self.link = link
     self.attempts = 0
     self.crawl_id = crawl_id
