4154d483a18566b155369511f6d6f932dcfbb3b6
Marco Montagna <marcojoemontagna@gmail.com>
2016-02-23 16:51:01 -0800

    Test crawl specific depth limit.

--- test/test_crawler.py
+++ test/test_crawler.py
@@ -57,6 +57,21 @@ class TestCrawler(unittest.TestCase):
     crawler.crawl()
     self.assertEqual(5, len(mockHttp.responses)) # Should be 5 responses left
 
+  def test_stop_at_crawl_specific_depth_limits(self):
+    Q = LocalQueue()
+    S = SimpleSet()
+    Q.send([LinkCrawlRequest('https://test.com', depth_limit=3)])
+    responses = ['<a href="' + str(i) + '">l</a>' for i in range(0, 10)]
+    mockHttp = FakeHttp(responses)
+    crawler = SimpleCrawler(Q, S, mockHttp)
+    crawler.stop_on_empty()
+    fakeProcessor = AttrDict({'process' : MagicMock(), 'close': MagicMock()})
+    crawler.add_response_processor(fakeProcessor)
+
+    #Should consume 3 responses before hitting depth limit
+    crawler.crawl()
+    self.assertEqual(7, len(mockHttp.responses)) # Should be 5 responses left
+
 
 if __name__ == "__main__":
   suite = unittest.TestLoader().loadTestsFromTestCase(TestCrawler)
