427c3ecf74533313e789c355dd65c4266ed54ff5
Marco Montagna <marcojoemontagna@gmail.com>
2016-02-23 18:59:53 -0800

    Implement periodic s3 store buffer flush.
    This prevents output from sitting in the buffer
    forever, or at least until the process stops.

--- crawl/processors/S3.py
+++ crawl/processors/S3.py
@@ -1,12 +1,15 @@
 from crawl.processors.Abstract import AbstractProcessor
 from collections import defaultdict
 import uuid, cStringIO, gzip, json, boto3, os, datetime
+from time import time
 from urlparse import urlparse
 
 class S3Store(AbstractProcessor):
-  def __init__(self, bucket, buffer_size=1000, s3_bucket_object=None):
+  def __init__(self, bucket, buffer_size=1000, s3_bucket_object=None, periodic_flush=60):
     #domain.crawl_id.folder.name
     self.output = defaultdict(lambda: defaultdict(lambda : defaultdict(lambda : defaultdict(lambda : []))))
+    self.last_processed_times = defaultdict(lambda: defaultdict(lambda : time()))
+    self.flush_time = periodic_flush
     self.max_buffer_size = buffer_size
 
     self.bucket = bucket
@@ -15,21 +18,34 @@ class S3Store(AbstractProcessor):
     else:
       self.s3_bucket_object = boto3.resource('s3').Bucket(self.bucket)
 
+  def tick(self):
+    for domain, crawl_id in self.domains_and_crawls():
+      if (self.buffer_expired(domain, crawl_id)):
+        self.write(domain, crawl_id)
+
   def process(self, url, response):
     domain = urlparse(url).netloc
     for output in response.output:
       self.output[domain][response.request.crawl_id][output.folder][output.name].append((url, (output.content, response.accessed)))
-
+      self.last_processed_times[domain][response.request.crawl_id] = time()
     self.checkBuffer()
 
   def buffer_size(self, domain, crawl_id):
     source = self.output[domain][crawl_id]
     return max([len(source[key][key2]) for key, key2 in [(key, key2) for key in source for key2 in source[key]]])
 
+  def domains_and_crawls(self):
+    return [(domain, crawl_id) for domain in self.output for crawl_id in self.output[domain]]
+
+  def buffer_expired(self, domain, crawl_id):
+    if (time() - self.last_processed_times[domain][crawl_id] > self.flush_time):
+      return True
+    return False
+
   """ Check all buffers and flush if full. """
   def checkBuffer(self):
-    for domain, crawl_id in [(domain, crawl_id) for domain in self.output for crawl_id in self.output[domain]]:
-      if (self.buffer_size(domain, crawl_id) > self.max_buffer_size):
+    for domain, crawl_id in self.domains_and_crawls():
+      if (self.buffer_size(domain, crawl_id) > self.max_buffer_size or self.buffer_expired(domain, crawl_id)):
         self.write(domain, crawl_id)
 
   def write(self, group, crawl_id):
@@ -39,6 +55,7 @@ class S3Store(AbstractProcessor):
       content = json.dumps(dict([(x[0], x[1]) for x in self.output[group][crawl_id][folder][name]]))
       self.writeToS3(group, folder, name, crawl_id, content, object_ids[group + crawl_id])
       self.output[group][crawl_id][folder][name] = []
+    del self.output[group][crawl_id]
 
   def writeToS3(self, group, folder, name, crawl_id, content, object_id):
     key = os.path.join('crawl', group, crawl_id, folder, name + '.' + object_id + '.json.gz')
@@ -48,6 +65,5 @@ class S3Store(AbstractProcessor):
     self.s3_bucket_object.Object(key).put(Body=fgz.getvalue())
 
   def close(self):
-    for group in self.output:
-      for crawl_id in self.output[group]:
+    for group, crawl_id in self.domains_and_crawls():
           self.write(group, crawl_id)
