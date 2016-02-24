237901c433542db04821b274dfa336b54a49ad09
Marco Montagna <marcojoemontagna@gmail.com>
2016-02-23 22:37:55 -0800

    Refactor crawl.default.state to generate result from generalized RedisQueue functions.
    Previously the redis backed queue container methods which were aware of
    the contents of crawlrequest objects it was storing (which isn't a good
    idea.), gotta keep it general.

--- crawl/default/state/__init__.py
+++ crawl/default/state/__init__.py
@@ -1,16 +1,23 @@
-import argparse
+import argparse, json
 from crawl.queues.Redis import RedisQueue
-import pprint, json
+from collections import defaultdict
+from urlparse import urlparse
+
+def summarize_state(RD):
+  requests = RD.all_items()
+  state = defaultdict(lambda: {'in_flight' : 0, 'domains' : defaultdict(lambda: 0)})
+  for crawlRequest in requests:
+    state[crawlRequest.crawl_id]['in_flight'] += 1
+    state[crawlRequest.crawl_id]['domains'][urlparse(crawlRequest.link).netloc[:50]] +=1
+
+  for key in state.keys():
+    state[key]['domains'] = dict(state[key]['domains'])
+  return {'num_workers': RQ.number_clients(), 'in_flight': RQ.items_in_queues(), 'crawls' : dict(state)}
 
 parser = argparse.ArgumentParser(description='Add URLs to start a crawl')
 parser.add_argument('--name_space', default='default', help='The crawl namespace.')
 args = parser.parse_args()
 
 RQ = RedisQueue(name_space=args.name_space)
-pp = pprint.PrettyPrinter(indent=2)
 
-print RQ.number_clients(), 'workers'
-print RQ.items_in_queues(),
-#print pp.pprint(RQ.queue_states())
-
-print json.dumps(RQ.queue_states(), indent=2, separators=(',', ': '))
+print json.dumps(summarize_state(RQ), indent=2, separators=(',', ': '))
